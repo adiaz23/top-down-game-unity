4c3839bf5e6c937af62e85cd80c01cf28271482f
Arianna Diaz <aldiaz0223@gmail.com>
2025-06-06 11:47:38 -0400

    Add player movement with Input System

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,47 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Movement System")]
+    [SerializeField] private InputAction moveAction;
+    [SerializeField] private float speed = 4;
+
+    private Rigidbody2D rb;
+    private Vector2 lookDirection = new Vector2(1, 0);
+    private Vector2 currentInput;
+    private Animator animator;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        moveAction.Enable();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rb.position;
+        position = position + currentInput * speed * Time.deltaTime;
+        rb.MovePosition(position);
+    }
+    
+    void Movement()
+    {
+        Vector2 move = moveAction.ReadValue<Vector2>();
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        currentInput = move;
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
     }
 }
